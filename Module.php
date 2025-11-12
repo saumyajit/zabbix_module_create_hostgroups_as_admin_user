6267f6e4b77024da05d0652388a8329716143033
Saumyajit <saumyajit@users.noreply.github.com>
2025-11-12 20:13:08 +0530

    Add files via upload

---
+++ Module.php
@@ -0,0 +1,56 @@
+<?php
+/**
+  * 
+  *
+  * @version 1.0
+  * @author Saumyajt Pramanik <saumyajitp8@gmail.com>
+  * @license GNU Lesser General Public License v3.0
+  *
+  * You can redistribute this library and/or modify it under the terms of
+  * the GNU LGPL as published by the Free Software Foundation,
+  * either version 3 of the License, or any later version.
+  * However you must not change author and copyright information.
+  */
+
+declare(strict_types = 1);
+
+namespace Modules\Iahg;
+
+use APP;
+use CController as CAction;
+use CWebUser;
+use Zabbix\Core\CModule;
+
+/**
+ * Please see Core\CModule class for additional reference.
+ */
+class Module extends CModule {
+
+	/**
+	 * Initialize module.
+	 */
+	public function init(): void {
+		// guests and non-admins don't need this host group editor
+		if (CWebUser::isGuest() || CWebUser::getType() == USER_TYPE_ZABBIX_USER) {
+			return;
+		}
+		// Initialize main menu (CMenu class instance).
+		APP::Component()->get('menu.main')->findOrAdd(_('Data collection'))->getSubmenu()->add((new \CMenuItem(_('Create Host Groups (Admin)')))->setAction('iahg.create'));
+	}
+
+	/**
+	 * Event handler, triggered before executing the action.
+	 *
+	 * @param CAction $action  Action instance responsible for current request.
+	 */
+	public function onBeforeAction(CAction $action): void {
+	}
+
+	/**
+	 * Event handler, triggered on application exit.
+	 *
+	 * @param CAction $action  Action instance responsible for current request.
+	 */
+	public function onTerminate(CAction $action): void {
+	}
+}
---
+++ views/module.iahg.php
@@ -0,0 +1,56 @@
+<?php
+/**
+  *
+  *
+  * @version 1.0
+  * @author Saumyajit Pramanik <saumyajitp8@gmail.com>
+  * @license GNU Lesser General Public License v3.0
+  *
+  * You can redistribute this library and/or modify it under the terms of
+  * the GNU LGPL as published by the Free Software Foundation,
+  * either version 3 of the License, or any later version.
+  * However you must not change author and copyright information.  
+  */
+
+$widget = (new CHtmlPage())->setTitle(_('Create Host Groups (Admin)'));
+
+$form = (new CForm('post', (new CUrl('zabbix.php'))
+		->setArgument('action', 'iahg.create')
+		->getUrl())
+	)
+	->setName('hostgroupForm')
+	->setAttribute('aria-labeledby', ZBX_STYLE_PAGE_TITLE_SUBMENU);
+
+$type_select = (new CSelect('prefix'))
+	->setValue($data['prefix'])
+	->setFocusableElementId('label-type')
+	->addOptions(CSelect::createOptionsFromArray($data['groups']));
+
+$form_list = (new CFormList('hostgroupFormList'));
+$form_list->addRow(
+	(new CDiv(_('This form allows you to create HostGroups as Zabbix Admin (user) for HostGroups you have read/write access to.')))
+		->addClass('table-forms-separator')
+);
+$form_list->addRow(
+	(new CLabel(_('Group name'), 'name'))->setAsteriskMark(),
+	[
+		$type_select,
+		new CSpan(' / '),
+		(new CTextBox('name', $data['name']))
+			->setAttribute('autofocus', 'autofocus')
+			->setWidth(ZBX_TEXTAREA_STANDARD_WIDTH)
+			->setAriaRequired()
+	]
+);
+
+$tab = (new CTabView())->addTab('hostgroupTab', _('Host group'), $form_list);
+$tab->setFooter(makeFormFooter(
+	new CSubmit('add', _('Add'))
+));
+
+$form->addItem($tab);
+
+$widget->addItem($form);
+$widget->show();
+
+?>
